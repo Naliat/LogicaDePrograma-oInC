d0e134b389ae71f90e561595eb88ad11b18d9c52
Naliat <tailanoliveira584@gmail.com>
2023-03-29 12:27:45 -0300

    Descubra se o número inteiro é impar ou par

---
+++ imparorpar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int number;
+
+printf("Digite o número inteiro: ");
+scanf("%d", &number);
+    
+      if (number % 2 == 0){
+printf("PAR\n");
+    }  else{
+printf("IMPAR\n");
+    }
+}
