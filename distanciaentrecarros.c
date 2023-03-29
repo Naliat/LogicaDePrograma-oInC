bd7a62f5702d3588ea38edec87848bab7ad69b94
Naliat <tailanoliveira584@gmail.com>
2023-03-29 10:55:16 -0300

    Calcular distancia entre dois pontos

---
+++ distanciaentrecarros.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include  <math.h>
+
+int main(){
+    double x1, y1, x2, y2, distancia;
+    
+printf("Digite a coordenada X e coordenada Y do primeiro ponto: ");
+    scanf("%lf %lf", &x1, &y1);
+printf("Digite a coordenada X e coordenada Y do segundo ponto: ");
+    scanf("%lf %lf", &x2, &y2);
+
+distancia = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+printf("%.2lf\n", distancia);
+
+return 0;
+
+    
+    
+}
