9e4b0299b9a6907d7fd768feb39c990a3870af7f
Naliat <tailanoliveira584@gmail.com>
2023-03-29 12:38:16 -0300

    Programa para fazer a soma entre dois inteiros

---
+++ somav.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int num1, num2;
+	printf("Digite o primeiro numero:");
+	scanf("%d", &num1);
+		printf("Digite o segundo numero:");
+	scanf("%d", &num2);
+
+int soma = num1 + num2;
+
+printf(" A soma é: %d", soma);
+
+return 1;
+}
+
+
+
