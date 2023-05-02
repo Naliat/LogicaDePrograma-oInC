2fe9ccbb3aa0df895ed29298cddf6dc46f97134e
Naliat <tailanoliveira584@gmail.com>
2023-05-02 09:14:17 -0300

    programa para calcular um triangulo

---
+++ ladotriangulo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    float lado1, lado2, lado3;
+
+    printf("Digite as medidas dos lados do triangulo: ");
+    scanf("%f %f %f", &lado1, &lado2, &lado3);
+
+    if (lado1 == lado2 && lado2 == lado3)
+    {
+        printf("Triangulo equilatero\n");
+    }
+    else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+    {
+        printf("Triangulo isosceles\n");
+    }
+    else
+    {
+        printf("Triangulo escaleno\n");
+    }
+
+    return 0;
+}
